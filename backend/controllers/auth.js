89cdf94a497ce319641484f7b555c804711ca3d1
neo <chojs990222@gmail.com>
2022-11-29 21:15:05 +0900

    test code

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,4 +1,4 @@
-const { validationResult } = require("express-validator/check");
+const { validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
@@ -56,7 +56,7 @@ exports.login = async (req, res, next) => {
         email: loadedUser.email,
         userId: loadedUser._id.toString(),
       },
-      "somesupersecretsecret",
+      "secret",
       { expiresIn: "1h" }
     );
     res.status(200).json({ token: token, userId: loadedUser._id.toString() });
